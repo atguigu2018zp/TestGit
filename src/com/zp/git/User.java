303a7a9ef6bdfb9d34a0d54d273727b69c103d20
eclipse_name <zp@qq.com>
2018-12-06 19:55:19 +0800

    insert Userproject

--- src/com/zp/git/User.java
+++ src/com/zp/git/User.java
@@ -1,5 +1,31 @@
 package com.zp.git;
 
 public class User {
+	private int id;
+	private String name;
+	private String passwodr;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPasswodr() {
+		return passwodr;
+	}
+
+	public void setPasswodr(String passwodr) {
+		this.passwodr = passwodr;
+	}
 }
